56e65228e8d54e718f57ca3c2c00e75350599b03
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-02-19 09:57:04 +0800

    Fuck it. The app actually working but the test won't pass

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,25 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [ breakLength, setBreakLength ] = useState(5)
-  const [ sessionLength, setSessionLength ] = useState(25) // in minutes
-  const [ timer, setTimer ] = useState(sessionLength) // in seconds
-  const [ timerDisplay, setTimerDisplay ] = useState(0)
+  const [ breakLength, setBreakLength ]           = useState(5)
+  const [ sessionLength, setSessionLength ]       = useState(25) // in minutes
+  const [ timer, setTimer ]                       = useState(sessionLength) // in seconds
+  const [ timerDisplay, setTimerDisplay ]         = useState(0)
   const [ sessionCountdown, setSessionCountdown ] = useState(false)
-  const [ breakCountdown, setBreakCountdown ] = useState(false)
-  const [ play, setPlay ] = useState(false)
-  const [ debug, setDebug ] = useState(true)
-  const [ intervalId, setIntervalId ] = useState(null)
-  const [ timerLabel, setTimerLable ] = useState("Session")
-  const [ sessionTimerId, setSessionTimerId ] = useState(null)
-  const [ breakTimerId, setBreakTimerId ] = useState(null)
+  const [ breakCountdown, setBreakCountdown ]     = useState(false)
+  const [ play, setPlay ]                         = useState(false)
+  const [ debug, setDebug ]                       = useState(true)
+  const [ intervalId, setIntervalId ]             = useState(null)
+  const [ timerLabel, setTimerLabel ]             = useState("Session")
+  const [ sessionTimerId, setSessionTimerId ]     = useState(null)
+  const [ breakTimerId, setBreakTimerId ]         = useState(null)
   let audioBeep = null
 
   const convertTimer = (time) => time*60
-  const formatTimerForDiplay = (time) => `${Math.floor(time/60)}:${String(time%60).padStart(2,'0')}`
-  // const formatTimerForDiplay = (time) => `${time}:00`
-
-  // const setLength = (type, op) => {
-  //   switch (type) {
-  //     case "session":
-  //       switch (op) {
-  //         case "increment":
-  //           setSessionLength(sessionLength => sessionLength + 1)
-  //           break
-  //         case "decrement":
-  //           setSessionLength(sessionLength => sessionLength - 1)
-  //           break
-  //       }
-  //     case "break":
-  //       switch (op) {
-  //         case "increment":
-  //           setBreakLength(breakLength => {
-  //             if (breakLength >= 0 && breakLength <= 60) {  breakLength+1 }
-  //
-  //           })
-  //       }
-  //   }
-  //   // if (type == "session") {
-  //   //   setSessionLength(sessionLength => sessionLength + 1)
-  //   // } else if (type == "break") {
-  //   //   if (op )
-  // }
+  const formatTimerForDiplay = (time) => `${String(Math.floor(time/60)).padStart(2,'0')}:${String(time%60).padStart(2,'0')}`
 
   const createTimer = (event) => {
-    const ID = setInterval(reduceTimer, 1000)
+    const ID = setInterval(reduceTimer, 500)
     setIntervalId(ID)
     console.log("createTimer", event)
   }
@@ -64,13 +37,15 @@ function App() {
   const reset = () => {
     clearTimer()
     audioBeep.pause()
+    audioBeep.currentTime = 0;
+    setBreakLength(5)
+    setSessionLength(25)
+    setTimer(convertTimer(25))
+
     setSessionCountdown(false)
     setBreakCountdown(false)
     setIntervalId(null)
     setPlay(false)
-    setBreakLength(5)
-    setSessionLength(25)
-    setTimer(convertTimer(25))
   }
 
   useEffect(() => {
@@ -80,58 +55,82 @@ function App() {
       console.log("play beep")
       setTimerDisplay(formatTimerForDiplay(timer))
       audioBeep.play()
+
+      clearTimer()
       // clearInterval(intervalId)
-      if (sessionCountdown) { setTimer(sessionLength) }
-      else { setTimer(breakLength) }
-      setSessionCountdown(!sessionCountdown)
-      setBreakCountdown(!breakCountdown)
+      setTimeout(() => {
+        if (sessionCountdown) {
+          setTimerLabel("Break")
+          setTimer(convertTimer(breakLength))
+          createTimer("timer watcher: break from session")
+        }
+        else {
+          setTimerLabel("Session")
+          setTimer(convertTimer(sessionLength))
+          createTimer("timer watcher: session from break")
+        }
+
+        setSessionCountdown(!sessionCountdown)
+        setBreakCountdown(!breakCountdown)
+      }, 100)
+
     } else {
       clearInterval(intervalId)
       // setIntervalId(null)
       console.log("timer_watcher: fatal error", timer)
     }
+    // const intervalId = setInterval(modifyTimer, 1000)
+    // return () => clearInterval(intervalId)
   }, [timer])
 
   useEffect(() => {
     setTimer(convertTimer(sessionLength))
   }, [sessionLength])
 
-  useEffect(() => {
-    console.log("play watcher", play?'play':'pause')
-    if (play && timer > 0) {
-      if (!sessionCountdown && !breakCountdown) {
-        setSessionCountdown(true)
-      } else if (sessionCountdown || breakCountdown) {
-        console.log("play pause resume")
-        const ID = setInterval(reduceTimer, 100)
-        setIntervalId(ID)
-      }
-    } else if (!play) {
-      console.log("play watcher else")
-      clearInterval(intervalId)
-    }
-    // const intervalId = setInterval(modifyTimer, 1000)
-    return () => clearInterval(intervalId)
-  }, [play])
+  // useEffect(() => {
+  //   // console.log("play watcher", play?'play':'pause')
+  //   if (play && timer > 0) {
+  //     if (!sessionCountdown && !breakCountdown) {
+  //       setSessionCountdown(true)
+  //       // createTimer("both off, first start")
+  //     } else if (sessionCountdown || breakCountdown) {
+  //       console.log("play pause resume")
+  //       createTimer("resume")
+  //     }
+  //   } else if (!play) {
+  //     console.log("play watcher else")
+  //     clearTimer()
+  //   }
+  //   // const intervalId = setInterval(modifyTimer, 1000)
+  //   // return () => clearInterval(intervalId)
+  // }, [play])
 
   // useEffect(() => setSessionCountdown(true),[])
-  useEffect(() => {
-    console.log("session watcher")
-    if (sessionCountdown) { setTimer(convertTimer(sessionLength)); createTimer() }
-    else { clearTimer(intervalId) }
-  }, [sessionCountdown])
 
-  useEffect(() => {
-    console.log("break watcher")
-    if (breakCountdown) { setTimer(convertTimer(breakLength)); createTimer() }
-    else { clearTimer(intervalId) }
-  }, [breakCountdown])
+  // useEffect(() => {
+  //   console.log("session watcher")
+  //   if (sessionCountdown) { setTimer(convertTimer(sessionLength)); createTimer("session watcher"); setTimerLabel("Session") }
+  //   else { clearTimer(intervalId) }
+  // }, [sessionCountdown])
+
+  // useEffect(() => {
+  //   console.log("break watcher")
+  //   if (breakCountdown) { setTimer(convertTimer(breakLength)); createTimer("break watcher"); setTimerLabel("Break") }
+  //   else { clearTimer(intervalId) }
+  // }, [breakCountdown])
 
   return (
     <div className="App">
       <main id="clock" className="App-header flex flex-col w-screen">
         <h1 className="text-5xl"><i className="fas fa-stopwatch"></i> Tomato clock?</h1>
-        <Settings breakLength={breakLength} sessionLength={sessionLength} setBreakLength={setBreakLength} setSessionLength={setSessionLength} />
+
+        <Settings
+          breakLength={breakLength}
+          sessionLength={sessionLength}
+          setBreakLength={setBreakLength}
+          setSessionLength={setSessionLength}
+        />
+
         <div id="timer" className="bg-gradient-to-b from-gray-500 to-gray-700 p-10 border border-gray-800 rounded-full mt-5 h-80 w-80 flex flex-col justify-center shadow-lg">
           <h3 id="timer-label">{ timerLabel }</h3>
           <h2 id="time-left" className="text-8xl">{timerDisplay}</h2>
@@ -140,14 +139,25 @@ function App() {
               id="start_stop"
               className="mr-5"
               onClick={ () => {
-                // handleStartStop(true)
-                setPlay(!play)
-                // handleStartStop(play)
-                }
-              }
+                  if (!play && !sessionCountdown && !breakCountdown) {
+                    setSessionCountdown(true)
+                    createTimer("first timer start button")
+                    setPlay(true)
+                    // createTimer("both off, first start")
+                  } else if (!play && (sessionCountdown || breakCountdown)) {
+                    console.log("play pause resume")
+                    setPlay(true)
+                    createTimer("resume")
+                  } else if (play) {
+                    console.log("play watcher else")
+                    clearTimer()
+                    setPlay(false)
+                  }
+              }}
             >
               <i className={`fas ${!play?'fa-play':'fa-pause'}`}></i>
             </button>
+
             <button
               id="reset"
               className="ml-5"
@@ -155,7 +165,15 @@ function App() {
             >
             <i className="fas fa-redo"></i>
             </button>
-            {/* <button><i className="fas fa-hourglass"></i></button> */}
+
+            <button
+              id="resetto"
+              className="ml-10"
+              onClick={ () => { setSessionLength(1); setBreakLength(1) } }
+            >
+            <i className="fas fa-wheelchair"></i>
+            </button>
+
           </div>
         </div>
         <audio
@@ -166,6 +184,7 @@ function App() {
           }}
           src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
         />
+
         <button className="mt-5 text-base" onClick={()=>setDebug(debug => !debug)}>DEBUG</button>
         { debug?
         <div id="debug" className="text-base">
@@ -196,7 +215,7 @@ const Settings = ({breakLength, setBreakLength, sessionLength, setSessionLength}
               id="break-increment"
               className="setting-button"
               onClick={ () => {
-                if (breakLength >= 0 && breakLength < 60)  {
+                if (breakLength < 60)  {
                   setBreakLength(breakLength => breakLength+1)
                 }
               }}
@@ -207,7 +226,7 @@ const Settings = ({breakLength, setBreakLength, sessionLength, setSessionLength}
               id="break-decrement"
               className="setting-button"
               onClick={ () => {
-                if (breakLength > 0 && breakLength <= 60)  {
+                if (breakLength > 1)  {
                   setBreakLength(breakLength => breakLength-1)
                 }
               }}
@@ -231,7 +250,7 @@ const Settings = ({breakLength, setBreakLength, sessionLength, setSessionLength}
               id="session-increment"
               className="setting-button"
               onClick={ () => {
-                if (sessionLength >= 0 && sessionLength < 60)  {
+                if (sessionLength < 60)  {
                   setSessionLength(sessionLength => sessionLength+1)
                 }
               }}
@@ -242,7 +261,7 @@ const Settings = ({breakLength, setBreakLength, sessionLength, setSessionLength}
               id="session-decrement"
               className="setting-button"
               onClick={ () => {
-                if (sessionLength > 0 && sessionLength <= 60)  {
+                if (sessionLength > 1)  {
                   setSessionLength(sessionLength => sessionLength-1)
                 }
               }}
